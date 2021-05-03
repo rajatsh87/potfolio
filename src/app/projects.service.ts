8c016d970719b44af79c8518d9d1549a7fa0abef
unknown <rajatsh87@gmail.com>
2021-05-04 01:08:52 +0530

    added projects

--- src/app/projects.service.ts
+++ src/app/projects.service.ts
@@ -6,40 +6,25 @@ import { Injectable } from '@angular/core';
 export class ProjectsService {
   projects=[
     {
-      'name':'protfolio',
-      'technologies used':'angular',
-      'features':'this is to list all the projects that i haved worked upon',
-      'URL':'#'
+      'name':'Protfolio',
+      'technologies':['angular','nodeJs'],
+      'discription':'usage of Angular material and NodeJs for triggering emails',
+      'URL':'https://rajatsahu-portfolio.netlify.app',
+      'image':'../../assets/images/portfolio.png'
     },
     {
-      'name':'protfolio',
-      'technologies used':'angular',
-      'features':'this is to list all the projects that i haved worked upon',
-      'URL':'#'
+      'name':'Rrecipe Book',
+      'technologies':['angular','firebase'],
+      'discription':'To implement all angular concepts i learn in Forms, Routes, Authentication and Data Mangement',
+      'URL':'https://app-recipe-store.herokuapp.com/',
+      'image':'../../assets/images/recipe.png'
     },
     {
-      'name':'protfolio',
-      'technologies used':'angular',
-      'features':'this is to list all the projects that i haved worked upon',
-      'URL':'#'
-    },
-    {
-      'name':'protfolio',
-      'technologies used':'angular',
-      'features':'this is to list all the projects that i haved worked upon',
-      'URL':'#'
-    },
-    {
-      'name':'protfolio',
-      'technologies used':'angular',
-      'features':'this is to list all the projects that i haved worked upon',
-      'URL':'#'
-    },
-    {
-      'name':'protfolio',
-      'technologies used':'angular',
-      'features':'this is to list all the projects that i haved worked upon',
-      'URL':'#'
+      'name':'shop-prototype',
+      'technologies':['angular','firebase'],
+      'discription':'aim to develop and create portal that could be used in a real life senario',
+      'URL':'https://shubham-vastralaye.netlify.app',
+      'image':'../../assets/images/shop.png'
     },
   ]
 
